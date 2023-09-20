9639061597d7f96da0ef513a1b62c1b9c14be937
Rostom-Chagunava <145435217+Rostom-Chagunava@users.noreply.github.com>
2023-09-20 11:36:11 +0200

    Add files via upload

---
+++ dey2/lessen2.py
@@ -0,0 +1,8 @@
+name = "rostom"
+surname = "chagunava"
+heigh = 180.1
+weigh = 78
+age = 29
+knows_programming = False
+knows_Georgianlanguege = True
+print(name + " " + surname + " " + str(heigh) + " " + str(weigh) + " " + str(age) + " " + str(knows_programming) + " " + str(knows_Georgianlanguege))
